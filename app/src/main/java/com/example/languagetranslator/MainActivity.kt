6d7ffddda4567f2c5ebca6376ec7e38d48c79a31
nukar1 <nukanukaroy@hotmail.com>
2020-04-25 13:50:22 +1200

    skeleton for splash screen

--- app/src/main/java/com/example/languagetranslator/MainActivity.kt
+++ app/src/main/java/com/example/languagetranslator/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.languagetranslator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT = 3000L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Handler().postDelayed(
+            {
+                val i = Intent(this@MainActivity, SplashScreen::class.java)
+                startActivity(i)
+                finish()
+            }, SPLASH_TIME_OUT)
     }
 }
