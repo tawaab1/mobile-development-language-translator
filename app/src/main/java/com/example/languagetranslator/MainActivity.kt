131032521c1d6856e6a7952e73de1d1d51d7ab65
nukar1 <nukanukaroy@hotmail.com>
2020-04-25 23:21:58 +1200

    start button to playscreen working

--- app/src/main/java/com/example/languagetranslator/MainActivity.kt
+++ app/src/main/java/com/example/languagetranslator/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.languagetranslator
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -10,6 +11,8 @@ import android.widget.Toolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_bottom_navigation.*
 import android.os.Handler
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,12 +20,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        
+        
+        startButton.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this@MainActivity, PlayScreenActivity::class.java)
+            startActivity(intent) })
     }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
@@ -37,4 +46,5 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+    
 }
--- app/src/main/java/com/example/languagetranslator/PlayScreenActivity.kt
+++ app/src/main/java/com/example/languagetranslator/PlayScreenActivity.kt
@@ -3,6 +3,7 @@ package com.example.languagetranslator
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_bottom_navigation.*
 
 import kotlinx.android.synthetic.main.activity_playscreen.*
 
@@ -12,6 +13,7 @@ class PlayScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_playscreen)
         setSupportActionBar(toolbar)
+
 
     }
 
