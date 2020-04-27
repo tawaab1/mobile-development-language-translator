5c9cf433fae0896b79f04d433f50193ecad24b09
Aisea Tawake <tawaab1@student.op.ac.nz>
2020-04-27 17:24:52 +1200

    fixing the about us linkage, navigation so every page is linked thus far

--- app/src/main/java/com/amorjk1/languagetranslator/MainActivity.kt
+++ app/src/main/java/com/amorjk1/languagetranslator/MainActivity.kt
@@ -6,35 +6,32 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_bottom_navigation.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-
     //lateinit var mTTs:TextToSpeech
-    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar1)
+        setSupportActionBar(toolbar)
 
-        
+        /**
+         * calling the class OnNavigationItemSelectedListener() to the onCreate() method
+         */
+        bnv.setOnNavigationItemSelectedListener(OnNavigationItemSelectedListener())
+        bnv.menu.getItem(1).isChecked = true
         //speak button
-
-        
         //val speakBtn = findViewById<View>(R.id.buttonSpeak)
         //speakBtn.setOnClickListener(YoutubeButtonOnClickListener())
-
         //val editText = findViewById<View>(R.id.editText)
-        
-        
-        
-        
         startButton.setOnClickListener(View.OnClickListener {
             val intent = Intent(this@MainActivity, PlayScreenActivity::class.java)
-            startActivity(intent) })
+            startActivity(intent)
+        })
     }
-    
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
@@ -47,13 +44,6 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             /**
-             * navigates to the BottomNavigationActivity
-             */
-            R.id.action_search -> {
-                startActivity(Intent(this@MainActivity, BottomNavigationActivity::class.java))
-                true
-            }
-            /**
              * navigates to the SettingsActivity
              */
             R.id.action_settings -> {
@@ -63,5 +53,35 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
-    
+    inner class OnNavigationItemSelectedListener : BottomNavigationView.OnNavigationItemSelectedListener {
+        override fun onNavigationItemSelected(item: MenuItem): Boolean {
+            return when (item.itemId) {
+                /**
+                 * navigates to the MainActivity
+                 */
+                R.id.navigation_home -> {
+                    startActivity(Intent(this@MainActivity, MainActivity::class.java))
+                    finish()
+                    true
+                }
+                /**
+                 * navigates to the About Us activity
+                 */
+                R.id.navigation_aboutus -> {
+                    startActivity(Intent(this@MainActivity, AboutUsActivity::class.java))
+                    finish()
+                    true
+                }
+                /**
+                 * navigates to the About Us activity
+                 */
+                R.id.navigation_playquiz->{
+                    startActivity(Intent(this@MainActivity, PlayScreenActivity::class.java))
+                    finish()
+                    true
+                }
+                else -> onNavigationItemSelected(item)
+            }
+        }
+    }
 }
