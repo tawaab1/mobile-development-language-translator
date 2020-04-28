69b612c3662e6958b029c44e0df3fbe1ddaed57f
nukar1 <nukanukaroy@hotmail.com>
2020-04-29 04:06:47 +1200

    list of questions added

--- app/src/main/java/com/amorjk1/languagetranslator/PlayScreenActivity.kt
+++ app/src/main/java/com/amorjk1/languagetranslator/PlayScreenActivity.kt
@@ -3,6 +3,7 @@ package com.amorjk1.languagetranslator
 import android.content.Intent
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
@@ -12,14 +13,33 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 import kotlinx.android.synthetic.main.activity_playscreen.*
 import java.util.*
+import kotlin.collections.ArrayList
 
 
 class PlayScreenActivity : AppCompatActivity() {
 
     lateinit var mTTs: TextToSpeech
+    lateinit var currentQuestion: Question
+    var questionNumber = 0
+    val maxNumQuestions = 5
+    lateinit var qAnswers:ArrayList<String>
+    lateinit var selectedQuestion:String
+
+    var questionsArray = arrayListOf<Question>(
+        Question(theQuestion = "How many Islands in New Zealand", theAnswer = arrayListOf("1","2","3","4")),
+        Question(theQuestion = "Who is the prime minister of nz", theAnswer = arrayListOf("brown","yellow","black","green")),
+        Question(theQuestion = "Who am i", theAnswer = arrayListOf("John","Sam","Roy","Chris")),
+        Question(theQuestion = "Jesus is?", theAnswer = arrayListOf("King","King2","King3","King6")),
+        Question(theQuestion = "How tall am i?", theAnswer = arrayListOf("tall","taller","tall-ish","tallest"))
+
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_playscreen)
+
+
+
         setSupportActionBar(toolbar)
         /**
          * calling the class OnNavigationItemSelectedListener() to the onCreate() method
@@ -85,9 +105,19 @@ class PlayScreenActivity : AppCompatActivity() {
                 }
                 else -> onNavigationItemSelected(item)
             }
-
         }
+    }
 
+    fun setQuestion(){
+        currentQuestion = questionsArray[questionNumber]
+        qAnswers = ArrayList(currentQuestion.theAnswer)
+        qAnswers.shuffle()
+    }
+
+    fun randomQuestion(){
+        questionsArray.shuffle()
+        setQuestion()
+        //Log.d("ANSWER",qAnswers[0],qAnswers[1],qAnswers[2],qAnswers[3])
     }
 
     override fun onBackPressed() {
--- app/src/main/java/com/amorjk1/languagetranslator/Question.kt
+++ app/src/main/java/com/amorjk1/languagetranslator/Question.kt
@@ -1,4 +1,21 @@
 package com.amorjk1.languagetranslator
 
-class Question {
+//class Question {
+//    var id:Int = 0
+//    var questionText:String? = null
+//    var questionImage:String? = null
+//    var answer1:String? = null
+//    var answer2:String? = null
+//    var answer3:String? = null
+//    var answer4:String? = null
+//    var correctAnswer:String? = null
+//    var imageQuestion: Boolean = false
+//    var categoryID:Int = 0
+//}
+
+data class Question(
+    var theQuestion:String, var theAnswer: ArrayList<String>
+)
+{
+
 }
